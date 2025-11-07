82f3e3be83443a77bdbf0bc804303f911c585640
jasonxwxia <jasonxwxia@gmail.com>
2025-11-07 17:46:54 -0500

    general content outline, responsive

--- scrollscript.js
+++ scrollscript.js
@@ -1,9 +1,12 @@
 const content = document.getElementsByClassName('scrollcontent');
 
+
 window.addEventListener('scroll', () => {
-	const scrollY = window.scrollY; 
+	const scrollY = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 500;
+	//const scrollY = window.scrollY/(window.innerHeight+val/5)*100;
+
 	for (let i=0; i<content.length; i++) {
-		content[i].style.transform = `translate(calc(-50% - ${scrollY}px + ${i}*500px))`;
+		content[i].style.transform = `translate(calc(-50% - ${scrollY}vw + ${i}*100vw))`;
 	}
 });
 
