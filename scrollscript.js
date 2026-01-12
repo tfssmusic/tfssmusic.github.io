aa6d1a6eeb77c54e23b51260e6b3e0fbedcc86e5
jasonxwxia <jasonxwxia@gmail.com>
2026-01-11 22:33:11 -0500

    fixed scroll snap error

--- scrollscript.js
+++ scrollscript.js
@@ -1,14 +1,19 @@
 const content = document.getElementsByClassName('scrollcontent');
 const ctparent = document.getElementsByClassName('contentparent');
 const bg = document.getElementsByClassName('staticbg');
+let justscrolled = false;
 
 
 ctparent[0].addEventListener('scroll', () => {
+	justscrolled = true;
 	let scrollY = (ctparent[0].scrollTop / (window.innerHeight*5)) * 500;
 	//const scrollY = window.scrollY/(window.innerHeight+val/5)*100;
 
-	if (scrollY % 100 > 97.5) {
+	if (scrollY % 100 > 97.5 && window.innerHeight >= 789) {
 		scrollY = (window.innerHeight*Math.ceil(ctparent[0].scrollTop/window.innerHeight) / (window.innerHeight*5)) * 500;
+	}
+	if (scrollY % 100 < 9 && window.innerHeight < 789) {
+		scrollY = (window.innerHeight*Math.floor(ctparent[0].scrollTop/window.innerHeight) / (window.innerHeight*5)) * 500;
 	}
 	console.log(scrollY, ctparent[0].scrollTop, ctparent[0].scrollHeight, window.innerHeight);
 
@@ -18,7 +23,7 @@ ctparent[0].addEventListener('scroll', () => {
 	for (let i=0; i<bg.length; i++) {
 		bg[i].style.transform = `translate(calc(-50% + ${scrollY}vw + ${i}*100vw))`;
 	}
-	
+	justscrolled = false;
 
 	/*
 	if ((ctparent[0].scrollTop/window.innerHeight-Math.floor(ctparent[0].scrollTop/window.innerHeight))>0.97){
