2a4ae18cb905da4f63df020ac3bd078fb6d8e0bc
jasonxwxia <jasonxwxia@gmail.com>
2026-01-11 22:12:14 -0500

    added background, more or less added functioning scroll snap for pseudo-horizontal scrolling

--- scrollscript.js
+++ scrollscript.js
@@ -1,12 +1,28 @@
 const content = document.getElementsByClassName('scrollcontent');
+const ctparent = document.getElementsByClassName('contentparent');
+const bg = document.getElementsByClassName('staticbg');
 
 
-window.addEventListener('scroll', () => {
-	const scrollY = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 500;
+ctparent[0].addEventListener('scroll', () => {
+	let scrollY = (ctparent[0].scrollTop / (window.innerHeight*5)) * 500;
 	//const scrollY = window.scrollY/(window.innerHeight+val/5)*100;
+
+	if (scrollY % 100 > 97.5) {
+		scrollY = (window.innerHeight*Math.ceil(ctparent[0].scrollTop/window.innerHeight) / (window.innerHeight*5)) * 500;
+	}
+	console.log(scrollY, ctparent[0].scrollTop, ctparent[0].scrollHeight, window.innerHeight);
 
 	for (let i=0; i<content.length; i++) {
 		content[i].style.transform = `translate(calc(-50% - ${scrollY}vw + ${i}*100vw))`;
 	}
+	for (let i=0; i<bg.length; i++) {
+		bg[i].style.transform = `translate(calc(-50% + ${scrollY}vw + ${i}*100vw))`;
+	}
+	
+
+	/*
+	if ((ctparent[0].scrollTop/window.innerHeight-Math.floor(ctparent[0].scrollTop/window.innerHeight))>0.97){
+		scrollY =  
+	*/
 });
 
