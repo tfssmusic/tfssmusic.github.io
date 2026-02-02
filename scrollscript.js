708ddc85e697aebfcd6f56dd39204ad5d03baadb
jasonxwxia <jasonxwxia@gmail.com>
2026-02-02 18:00:11 -0500

    added spotlight, fixed scroll

--- scrollscript.js
+++ scrollscript.js
@@ -1,21 +1,50 @@
-const content = document.getElementsByClassName('scrollcontent');
-const ctparent = document.getElementsByClassName('contentparent');
-const bg = document.getElementsByClassName('staticbg');
-let justscrolled = false;
+// const content = document.getElementsByClassName('scrollcontent');
+// const ctparent = document.getElementsByClassName('contentparent');
+// const bg = document.getElementsByClassName('staticbg');
+// 
+// 
+// ctparent[0].addEventListener('scroll', () => {
+// 	let scrollY = (ctparent[0].scrollTop / (window.innerHeight*5)) * 500;
+// 	//const scrollY = window.scrollY/(window.innerHeight+val/5)*100;
+// 
+// 	if (scrollY % 100 > 97.5 && window.innerHeight >= 789) {
+// 		scrollY = (window.innerHeight*Math.ceil(ctparent[0].scrollTop/window.innerHeight) / (window.innerHeight*5)) * 500;
+// 	}
+// 	if (scrollY % 100 < 5 && window.innerHeight < 789) {
+// 		scrollY = (window.innerHeight*Math.floor(ctparent[0].scrollTop/window.innerHeight) / (window.innerHeight*5)) * 500;
+// 	}
+// 	console.log(scrollY, ctparent[0].scrollTop, ctparent[0].scrollHeight, window.innerHeight);
+// 
+// 	for (let i=0; i<content.length; i++) {
+// 		content[i].style.transform = `translate(calc(-50% - ${scrollY}vw + ${i}*100vw))`;
+// 	}
+// 	for (let i=0; i<bg.length; i++) {
+// 		bg[i].style.transform = `translate(calc(-50% + ${scrollY}vw + ${i}*100vw))`;
+// 	}
+// 
+// });
 
 
-ctparent[0].addEventListener('scroll', () => {
-	justscrolled = true;
-	let scrollY = (ctparent[0].scrollTop / (window.innerHeight*5)) * 500;
-	//const scrollY = window.scrollY/(window.innerHeight+val/5)*100;
+const content = document.getElementsByClassName("scrollcontent");
+const bg = document.getElementsByClassName("staticbg");
 
-	if (scrollY % 100 > 97.5 && window.innerHeight >= 789) {
-		scrollY = (window.innerHeight*Math.ceil(ctparent[0].scrollTop/window.innerHeight) / (window.innerHeight*5)) * 500;
-	}
-	if (scrollY % 100 < 9 && window.innerHeight < 789) {
-		scrollY = (window.innerHeight*Math.floor(ctparent[0].scrollTop/window.innerHeight) / (window.innerHeight*5)) * 500;
-	}
-	console.log(scrollY, ctparent[0].scrollTop, ctparent[0].scrollHeight, window.innerHeight);
+const snapobj = document.getElementsByClassName("hiddencontent");
+
+
+let scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+let scrollY = (window.scrollY/scrollableHeight)*500;
+
+for (let i=0; i<snapobj.length; i++) {
+
+	snapobj[i].style.height = window.innerHeight+(scrollableHeight-window.innerHeight*5)/5+"px"; 
+
+}
+
+document.addEventListener("scroll", () => {
+	let scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+	let scrollY = (window.scrollY/scrollableHeight)*500;
+
+	console.log(scrollY, scrollableHeight, window.scrollY, window.innerHeight, document.documentElement.scrollHeight);
 
 	for (let i=0; i<content.length; i++) {
 		content[i].style.transform = `translate(calc(-50% - ${scrollY}vw + ${i}*100vw))`;
@@ -23,11 +52,4 @@ ctparent[0].addEventListener('scroll', () => {
 	for (let i=0; i<bg.length; i++) {
 		bg[i].style.transform = `translate(calc(-50% + ${scrollY}vw + ${i}*100vw))`;
 	}
-	justscrolled = false;
-
-	/*
-	if ((ctparent[0].scrollTop/window.innerHeight-Math.floor(ctparent[0].scrollTop/window.innerHeight))>0.97){
-		scrollY =  
-	*/
 });
-
---
+++ spotlight.js
@@ -0,0 +1,61 @@
+const layers = [
+	{ el: document.querySelector(".mask"), ease: 0.08, x: window.innerWidth/2, y: window.innerWidth/2 },
+	{ el: document.querySelector(".mask2"), ease: 0.08, x: window.innerWidth/2, y: window.innerWidth/2 }
+];
+
+let targetX = innerWidth / 2;
+let targetY = innerHeight / 2;
+
+
+const m1 = document.getElementsByClassName("imgwrap");
+
+window.addEventListener("mousemove", e => {
+	targetX = e.clientX;
+	targetY = e.clientY;
+});
+
+function animate() {
+	const l1 = layers[0];
+	const l2 = layers[1];
+	
+	l1.x += (targetX - l1.x) * l1.ease;
+	l1.y += (targetY - l1.y) * l1.ease;
+	l2.x += (targetX - l2.x) * l2.ease;
+	l2.y += (targetY - l2.y) * l2.ease;
+
+	l1.el.style.setProperty("--x", `${l1.x}px`);
+	l1.el.style.setProperty("--y", `${l1.y + 0.5*m1[1].offsetHeight}px`);
+	l2.el.style.setProperty("--x", `${l2.x}px`);
+	l2.el.style.setProperty("--y", `${l2.y - 3.5*m1[1].offsetHeight}px`);
+
+	
+	requestAnimationFrame(animate);
+};
+animate();
+
+
+
+//const layer = document.querySelector(".mask");
+//const layer2 = document.querySelector(".mask2");
+//
+//const m1 = document.getElementsByClassName("imgwrap");
+//
+//window.addEventListener('mousemove', (e) => {
+
+
+
+
+//const layer = document.querySelector(".mask");
+//const layer2 = document.querySelector(".mask2");
+//
+//const m1 = document.getElementsByClassName("imgwrap");
+//
+//window.addEventListener('mousemove', (e) => {
+//	layer.style.setProperty('--x', `${e.clientX}px`);
+//	layer.style.setProperty('--y', `${e.clientY+ 0.5*m1[1].offsetHeight}px`);
+//});
+//
+//window.addEventListener('mousemove', (e) => {
+//	layer2.style.setProperty('--x', `${e.clientX}px`);
+//	layer2.style.setProperty('--y', `${e.clientY-3.05*m1[1].offsetHeight}px`);
+//});
