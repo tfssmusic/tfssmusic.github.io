b360605a49c2a4b3a1466a367b918fcf232b33b6
jasonxwxia <jasonxwxia@gmail.com>
2026-03-04 08:43:16 -0500

    removed manual scroll restoration and made frame applicable to all posters

--- post.js
+++ post.js
@@ -1,16 +1,32 @@
 const event1 = document.getElementById("event1");
+const event2 = document.getElementById("event2");
+const event3 = document.getElementById("event3");
 const blocker = document.getElementById("blocker");
 const xbtn = document.getElementById("xbtn");
 const frame = document.getElementById("postframe");
 
 console.log("event1");
 
-event1.addEventListener("click", function(){
-	console.log("FUCK");
-	frame.style.display = "block";
+function hide(){
 	blocker.style.display = "block";
 	xbtn.style.display = "block";
+	frame.style.display = "block";
 	document.body.style.overflowY = "hidden";
+}
+
+event1.addEventListener("click", function(){
+	hide();
+	frame.src = "https://tfssmusic.github.io/Page";
+})
+
+event2.addEventListener("click", function(){
+	frame.src = "https://example.com";
+	hide();
+})
+
+event3.addEventListener("click", function(){
+	hide();
+	frame.src = "https://tfssmusic.github.io/events";
 })
 
 function restore(){
